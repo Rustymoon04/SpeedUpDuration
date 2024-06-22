34b2f8afc12ecdd6d7f61a72d9f5e36e46fba276
Rustymoon04 <jimboismydog@gmail.com>
2024-06-22 23:59:12 +0800

    SpeedUpDuration

---
+++ Assets/scripts.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scripts : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotSpeed;
+    public float originalSpeed;
+    public float powerUpDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move object forward along the z axis, 1 unit per sec
+        //Vector3.forward = Vector3(0,0,1)
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * moveSpeed));
+
+            //Alternative Code 
+            //transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S)) 
+        { 
+            transform.Translate(Vector3.back * (Time.deltaTime * moveSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * moveSpeed));
+            
+            //rotation
+            //transform.Rotate(Vector3.up, Time.deltaTime * rotSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * (Time.deltaTime * moveSpeed));
+
+            //rotation
+            //transform.Rotate(Vector3.down, Time.deltaTime + rotSpeed);
+        }
+
+    }
+
+private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            StartCoroutine(SpeedUp());
+            Destroy(other.gameObject);
+        }
+    }
+
+    private IEnumerator SpeedUp()
+    {
+        moveSpeed *= 2;
+        yield return new WaitForSeconds(powerUpDuration);
+        moveSpeed = originalSpeed;
+    }
+
+}
